9c8226b7d8b9307e67be669b1c285a66486218dd
Jasper <80272820+Code-Jap@users.noreply.github.com>
2021-05-09 00:10:05 +0200

    Add files via upload

---
+++ JavaScript_3/script.js
@@ -0,0 +1,36 @@
+const age = 18;
+
+if (age >= 18){
+
+    console.log("You may enter");
+
+} else {
+
+    console.log("You can not enter");
+
+}
+
+
+const isFemale = false;
+
+if (isFemale == true){
+
+    console.log("You're a female");
+
+} else {
+
+    console.log("you're a guy");
+}
+
+
+const driverSatus = "bob";
+
+if (driverSatus == "bob"){
+
+    console.log("You can drive");
+
+} else {
+
+    console.log("You can not drive");
+}
+
